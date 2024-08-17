c08829aed052a5b2b66232dcbd03c1d9b3c059cd
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2024-08-17 18:38:36 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Profile from "./pages/Auth/Profile"
-import SignUp from "./pages/Auth/SignUp"
-import SignIn from "./pages/Auth/SignIn"
-import Header from "./components/Header"
-import PrivateRoute from "./components/PrivateRoute"
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -21,45 +11,48 @@ import PrivateDashboard from "./pages/Developers/PrivateDashboard";
 import AdminEntryForm from "./pages/Developers/AdminEntryForm";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import StartPage from './pages/Exam_Section/StartPage';
-import ExamWindow from "./pages/Exam_Section/ExamWindow";
 import SubmitConfirmation from "./pages/Exam_Section/SubmitConfirmation";
 
 import './index.css';
->>>>>>> Stashed changes
+
+
+import ExamWindow from "./pages/Exam_Section/ExamWindow";
+import UserDashboard from "./pages/Users/UserDashboard";
+
 
 function App() {
+  const privatePassId = import.meta.env.VITE_PRIVATE_DASHBOARD_ID;
+
   return (
     <BrowserRouter>
-      {/* Header component */}
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-<<<<<<< Updated upstream
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-=======
-        <Route path='/admin-dashboard' element={<AdminDashboard />} />
+      <div className='pt-20'> {/* Adjusted padding-top to match header height */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/admin-dashboard' element={<AdminDashboard />} />
 
+          {/* for owners */}
+          <Route path={`/private-dashboard/${privatePassId}`} element={<PrivateDashboard />} />
+          <Route path={`/private-dashboard/${privatePassId}/admin-entry`} element={<AdminEntryForm />} />
 
-        <Route path={`/private-dashboard/${privatePassId}`} element={<PrivateDashboard />} />
-        <Route path={`/private-dashboard/${privatePassId}/admin-entry`} element={<AdminEntryForm />} />
+          <Route element={<PrivateRoute />}>
+            {/* For the user */}
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/registration-form' element={<RegistrationForm />} />
+            <Route path='/user-dashboard' element={<UserDashboard />} />
 
-
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/registration-form' element={<RegistrationForm />} />
-          <Route path="/start" element={<StartPage />} />
-          <Route path='/exam-section' element={<ExamWindow />} />
-          <Route path='/submit-confirmation' element={<SubmitConfirmation/>}/>
->>>>>>> Stashed changes
-        </Route>
-      </Routes>
+            {/* Exam section part */}
+            <Route path="/start" element={<StartPage />} />
+            <Route path='/exam-section' element={<ExamWindow />} />
+            <Route path='/submit-confirmation' element={<SubmitConfirmation />} />
+          </Route>
+        </Routes>
+      </div>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
